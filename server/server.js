ee3fcb4a4c03d949d26f08540187cef066f0468d
Adrian Maningding <amazingadrian@adrians-mbp.lan>
2022-11-02 14:11:10 -0700

    Mongoose Connected

--- server/server.js
+++ server/server.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mongoose = require('mongoose')
 
-const port = 8000; 
+require('dotenv').config();
+const { PORT = 8000, DATABASE_URL } = process.env;
+
+mongoose.connect(DATABASE_URL);
 
 //use middleware 
 
 app.use(cors());
 app.use(express.json());
 
-app.listen(port, ()=> {
-  console.log(`Server is running on port ${port}`)
+mongoose.connection
+.on('connected', () => console.log('Connected to MongoDB'))
+.on('error', (error) => console.log('MongoDB Error:' + error.message));
+
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the ExpenseTracker API');
+});
+
+app.listen(PORT, ()=> {
+  console.log(`Server is running on port ${PORT}`)
 })
